265b4e4dcfe8441ee6e109ebf93f3bc5d19b06c2
Griffin <griffinbkeeler@gmail.com>
2026-03-14 12:52:51 -0500

    Inital commit

---
+++ src/preprocess/preprocess.py
@@ -0,0 +1,31 @@
+
+
+def filter_raw(
+        raw,
+        sfreq,
+        l_freq,
+        h_freq
+):
+    """
+    Resamples and applies band-pass filter to the raw EEG data.
+
+    Parameters
+    ----------
+    raw : mne.RawArray
+        The MNE Raw object.
+    sfreq : int
+        The resampling rate.
+    l_freq : int
+        Low-pass frequency for band-pass filter.
+    h_freq  : int
+        High-pass frequency for band-pass filter
+
+    Returns
+    -------
+    mne.RawArray
+        The filtered MNE Raw object.
+    """
+    raw.resample(sfreq=sfreq)
+    raw.filter(l_freq=l_freq, h_freq=h_freq)
+
+    return raw
